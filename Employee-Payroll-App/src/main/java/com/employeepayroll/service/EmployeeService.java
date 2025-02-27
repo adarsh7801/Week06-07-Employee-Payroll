bb17187e5297a71429e1be4922500fe4c14ee078
Adarsh Gupta <adarshguptajhs@gmail.com>
2025-02-27 15:38:53 +0530

    [Adarsh Gupta] Add . Employee Service

---
+++ Employee-Payroll-App/src/main/java/com/employeepayroll/service/EmployeeService.java
@@ -0,0 +1,19 @@
+package com.employeepayroll.service;
+
+import com.employeepayroll.model.Employee;
+import com.employeepayroll.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+    @Autowired
+    private EmployeeRepository repository;
+
+    public List<Employee> getAllEmployees() { return repository.findAll(); }
+    public Employee getEmployeeById(Long id) { return repository.findById(id).orElse(null); }
+    public Employee saveEmployee(Employee employee) { return repository.save(employee); }
+    public void deleteEmployee(Long id) { repository.deleteById(id); }
+}
