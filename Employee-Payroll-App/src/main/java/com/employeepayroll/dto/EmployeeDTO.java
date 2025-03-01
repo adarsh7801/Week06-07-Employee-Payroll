c28fe42f49f1b17833efa43ad42e523b75c71ce6
Adarsh Gupta <adarshguptajhs@gmail.com>
2025-03-01 13:23:37 +0530

    [Adarsh Gupta] Add . Employee Data transfer object

--- Employee-Payroll-App/src/main/java/com/employeepayroll/dto/EmployeeDTO.java
+++ Employee-Payroll-App/src/main/java/com/employeepayroll/dto/EmployeeDTO.java
@@ -1,24 +1,39 @@
 package com.employeepayroll.dto;
 
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Pattern;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Getter
 @Setter
-public class EmployeeDTO {
+public @ToString class EmployeeDTO {
 
     @NotEmpty(message = "Name is required and cannot be empty")
     @Pattern(regexp = "^[A-Z][a-zA-Z\\s]{2,}$", message = "Name must start with a capital letter and have at least 3 characters")
     private String name;
 
+    @Min(value = 500, message = "Minimum wage should be more than 500")
     private double salary;
 
-    public EmployeeDTO() {}
+    @Pattern(regexp = "^(male|female)$", message = "Gender needs to be male or female")
+    private String gender;
 
-    public EmployeeDTO(String name, double salary) {
-        this.name = name;
-        this.salary = salary;
-    }
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    @NotNull (message = "startDate should not be empty")
+    @PastOrPresent (message = "startDate should be past or today date")
+    private LocalDate startDate;
+
+    @NotBlank(message = "Note cannot be Empty")
+    private String note;
+
+    @NotBlank (message = "Profile pic cannot be Empty")
+    private String profilePic;
+
+    @NotNull(message = "department should not be empty")
+    private List<String> department;
 }
