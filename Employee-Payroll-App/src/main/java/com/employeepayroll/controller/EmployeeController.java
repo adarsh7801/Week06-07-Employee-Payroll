6b17c59c2cc2f98402a86a5c24a47879c55489b0
Adarsh Gupta <adarshguptajhs@gmail.com>
2025-03-01 13:15:30 +0530

    [Adarsh Gupta] Add . Employee Controller for controlling CRUD operation by using SpringBoot

--- Employee-Payroll-App/src/main/java/com/employeepayroll/controller/EmployeeController.java
+++ Employee-Payroll-App/src/main/java/com/employeepayroll/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.employeepayroll.controller;
 import com.employeepayroll.dto.EmployeeDTO;
 import com.employeepayroll.model.Employee;
 import com.employeepayroll.service.EmployeeService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,18 +17,15 @@ class EmployeeController {
     private EmployeeService service;
 
     @GetMapping
-    public List<Employee> getAllEmployees() {
-        return service.getAllEmployees();
-    }
+    public List<Employee> getAllEmployees() { return service.getAllEmployees(); }
 
     @GetMapping("/{id}")
-    public Employee getEmployeeById(@PathVariable Long id) {
-        return service.getEmployeeById(id);
-    }
+    public Employee getEmployeeById(@PathVariable Long id) { return service.getEmployeeById(id); }
 
     @PostMapping
-    public Employee createEmployee(@RequestBody EmployeeDTO employee) {
-        return service.saveEmployee(employee);
+    public ResponseEntity<Employee> createEmployee(@Valid @RequestBody EmployeeDTO employeeDTO) {
+
+        return ResponseEntity.ok(service.saveEmployee(employeeDTO));
     }
 
     @PutMapping("/{id}")
