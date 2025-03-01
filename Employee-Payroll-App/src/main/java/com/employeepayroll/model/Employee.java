cc349a4c8269cd3786003413b8d4d87014ea1191
Adarsh Gupta <adarshguptajhs@gmail.com>
2025-03-01 11:40:30 +0530

    [Adarsh Gupta] Add . Employee Entity

--- Employee-Payroll-App/src/main/java/com/employeepayroll/model/Employee.java
+++ Employee-Payroll-App/src/main/java/com/employeepayroll/model/Employee.java
@@ -4,8 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,23 +24,12 @@ public class Employee {
         this.salary = salary;
     }
 
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public double getSalary() {
-        return salary;
-    }
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
+
+    /*// Getters and Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public double getSalary() { return salary; }
+    public void setSalary(double salary) { this.salary = salary; }*/
 }
